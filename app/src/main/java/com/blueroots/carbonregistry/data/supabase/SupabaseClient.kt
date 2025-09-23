b56e01e563d9905b28572f6b26fe09fed77c9da6
Pratik <pratiksalunke1905@gmail.com>
2025-09-23 20:49:23 +0530

    feat : logo updation

--- app/src/main/java/com/blueroots/carbonregistry/data/supabase/SupabaseClient.kt
+++ app/src/main/java/com/blueroots/carbonregistry/data/supabase/SupabaseClient.kt
@@ -11,8 +11,8 @@ import io.github.jan.supabase.storage.Storage
 object SupabaseClient {
 
     // Replace with your Supabase project URL and anon key
-    private const val SUPABASE_URL = "https://bsdevsetmnxsvbtsjrvs.supabase.co"
-    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzZGV2c2V0bW54c3ZidHNqcnZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NDEwOTYsImV4cCI6MjA3NDAxNzA5Nn0.WZ1PnwE86ZC1KPZVf_P_KML6slHsYJzQN_6VXR41GCg"
+    private const val SUPABASE_URL = "https://pvstimmfexsksbrovhsd.supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2c3RpbW1mZXhza3Nicm92aHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NzY2MjUsImV4cCI6MjA3NDA1MjYyNX0.FKhpFQvJEd3YTv-oIXvz03hJyrxue9ELim6LsGKSxU4"
 
     val client = createSupabaseClient(
         supabaseUrl = SUPABASE_URL,
